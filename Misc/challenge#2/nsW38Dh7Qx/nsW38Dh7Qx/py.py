2aef417b8aaddf04acf6cddaa805bc51291c915b
Emanuelle <emanuellejs1999@gmai.com>
2020-02-24 15:52:12 -0600

    Solution of challenge5 finish

--- Misc/challenge#2/nsW38Dh7Qx/nsW38Dh7Qx/py.py
+++ Misc/challenge#2/nsW38Dh7Qx/nsW38Dh7Qx/py.py
@@ -1,18 +1,16 @@
 from PIL import Image
-from PIL import ImageFilter
 
-new_image_data = Image.new('L', (640, 360),(0))
-data = list(new_image_data.getdata())
-new_image_data.show()
+img = Image.new('RGB', (640, 360),(255, 255, 255))
+pixels=img.load()
 
-
-for i in range(0,300):
+for i in range(300):
 	im = Image.open('nsW38Dh7Qx'+str(i)+'.jpg')
-	images_data = im.getdata()
-	for color in range(0,len(list(im.getdata()))):
-		print(images_data[color])
-		data[color] = data[color] + images_data[color]
+	pix = im.load()
+	for p in range(640):
+		for q in range(360):
+			pixel_value=pix[p,q]
+			if(pixel_value>249 and pixel_value<=256):
+				pixels[p,q]=0
+	im.close()
 
-new_image_data.putdata(data)
-new_image_data.show()
-#image1 = Image.open('image_name.extension')
+img.save('answer.jpg')
--- Stegnography/tools/documentation.py
+++ Stegnography/tools/documentation.py
@@ -90,3 +90,30 @@ print(image1.mode)
 
 # delete variables to manage the memory 
 del image1,image2,rotated_image
+
+
+
+
+
+-------------------------------------------
+Merge 300 black and white images 
+
+from PIL import Image
+
+img = Image.new('RGB', (640, 360),(255, 255, 255))
+pixels=img.load()
+
+for i in range(300):
+	im = Image.open('nsW38Dh7Qx'+str(i)+'.jpg')
+	pix = im.load()
+	for p in range(640):
+		for q in range(360):
+			pixel_value=pix[p,q]
+			if(pixel_value>249 and pixel_value<=256):
+				pixels[p,q]=0
+	im.close()
+
+img.save('answer.jpg')
+
+
+-------------------------------------------
